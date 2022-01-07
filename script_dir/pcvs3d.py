168a477cd4d5f03a08bff9a764a833f1799914d5
Michael de Lagarde <michael.delagarde@delair.local>
2022-01-07 21:34:49 +0100

    Difference between 2 point clouds

--- script_dir/pcvs3d.py
+++ script_dir/pcvs3d.py
@@ -5,74 +5,66 @@ from matplotlib import cm
 
 
 
-def pcvs3d():
+def pcvs3d(input_path, ref_path, work_dir):
 
-	f = laspy.read('../work_dir/input.las')
-	# f = laspy.read('./210323_Vol_6_Lidar.las')
-	# print(f.header.__dict__)
-	scale = f.header.scale
-	offset = f.header.offset
-	mins = f.header.mins
-	maxs = f.header.maxs
-
-	print('scale', scale)
-	print('offset', offset)
-
-	# print(f.header.vlrs)
-	# for i in range(len(f.header.vlrs)):
-	# 	print(f.header.vlrs[i].__dict__)
-
-	x = f.X * f.header.scales[0] + f.header.offsets[0]
-	y = f.Y * f.header.scales[1] + f.header.offsets[1]
-	z = f.Z * f.header.scales[2] + f.header.offsets[2]
-
-
-	# min_x = mins[0]
-	# min_y = mins[1]
-	# max_x = maxs[0]
-	# max_y = maxs[1]
-
-	# min_x = min(x)
-	# min_y = min(y)
-	# max_x = max(x)
-	# max_y = max(y)
+	fi = laspy.read(input_path)
+	x = fi.X * fi.header.scales[0] + fi.header.offsets[0]
+	y = fi.Y * fi.header.scales[1] + fi.header.offsets[1]
+	z = fi.Z * fi.header.scales[2] + fi.header.offsets[2]
 	xyz = np.vstack([x.astype(np.float32), y.astype(np.float32), z.astype(np.float32)]).transpose()
 
-	# xyz = xyz[0:1000]
-
-
-	# print(xyz)
+	fr = laspy.read(ref_path)
+	xr = fr.X * fr.header.scales[0] + fr.header.offsets[0]
+	yr = fr.Y * fr.header.scales[1] + fr.header.offsets[1]
+	zr = fr.Z * fr.header.scales[2] + fr.header.offsets[2]
+	xyz_ref = np.vstack([xr.astype(np.float32), yr.astype(np.float32), zr.astype(np.float32)]).transpose()
 
 	pcd = o3d.geometry.PointCloud()
 	pcd.points = o3d.utility.Vector3dVector(xyz)
 
-	# o3d.visualization.draw_geometries([pcd])
-
-
-	xyz_ref = xyz+[0,0,0.1]
-
 	pcd_ref = o3d.geometry.PointCloud()
-
-
 	pcd_ref.points = o3d.utility.Vector3dVector(xyz_ref)
 
-
 	dists = pcd_ref.compute_point_cloud_distance(pcd)
-	# print(dists)
 	dists = np.array(dists)
-	dists = dists/max(dists)
+	maxd = max(dists)
+	if maxd>0:
+		dists0 = dists/max(dists)
+	else:
+		dists0 = dists
 
-	cmap = cm.get_cmap('RdYlGn', 12)
-	colors = [cmap(dist)[0:3] for dist in dists]
-	# print(colors)
+	cmap = cm.get_cmap('jet')
+	colors = [cmap(dist)[0:3] for dist in dists0]
+	colors = np.array(colors)
 
-	pcd_diff = o3d.geometry.PointCloud()
-	pcd_diff.points = o3d.utility.Vector3dVector(xyz)
-	pcd_diff.colors = o3d.utility.Vector3dVector(colors)
+	# pcd_diff = o3d.geometry.PointCloud()
+	# pcd_diff.points = o3d.utility.Vector3dVector(xyz)
+	# pcd_diff.colors = o3d.utility.Vector3dVector(colors)
+	# o3d.visualization.draw_geometries([pcd_diff])
 
-	o3d.visualization.draw_geometries([pcd_diff])
+	fi.red = colors[:,0]
+	fi.green = colors[:,1]
+	fi.blue = colors[:,2]
+	fi.intensity = dists
+	fi.write(work_dir+'output.las')
+
+	# outFile1 = laspy.file.File(work_dir+'output.las', mode = "w", header = fi.header)
+	# outFile1.X = fi.X
+	# outFile1.Y = fi.Y
+	# outFile1.Z = fi.Z
+	# outFile1.red = colors[:,0]
+	# outFile1.green = colors[:,1]
+	# outFile1.blue = colors[:,2]
+	# outFile1.intensity = dists
+	# outFile1.close()
+
+
 
 
 if __name__ == "__main__":
 
-	pcvs3d()
+
+	input_path = '../work_dir/input.las'
+	ref_path = '../work_dir/input.las'
+	work_dir = '../work_dir/'
+	pcvs3d(input_path, ref_path, work_dir)
